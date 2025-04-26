3b55e7d541e418625a3700c938f6dda41040de31
Markus Rösner <markus.roesner@gmx.net>
2025-04-27 01:09:50 +0200

    fix sus questions

--- app/components/experiment/shared/QuestionnaireForm.tsx
+++ app/components/experiment/shared/QuestionnaireForm.tsx
@@ -311,16 +311,16 @@ export default function QuestionnaireForm({ onSubmit }: QuestionnaireFormProps) 
       },
       'sus': {
         questions: [
-          "The interface was intuitive to use.",
-          "I felt uncertain completing the tasks with this interface.",
-          "The information was presented in a clear and understandable way.",
-          "It was difficult to navigate through the different steps of each task.",
-          "The interface helped me complete the tasks efficiently.",
-          "I was unsure about what actions were expected of me during each task.",
-          "The visual design of the interface supported my decision-making.",
-          "The functionality of the interface did not meet my expectations.",
-          "The interface responded reliably to my inputs.",
-          "Overall, the interface provided a poor user experience."
+          "I think that I would like to use this system frequently.",
+          "I found the system unnecessarily complex.",
+          "I thought the system was easy to use.",
+          "I think that I would need the support of a technical person to be able to use this system.",
+          "I found the various functions in this system were well integrated.",
+          "I thought there was too much inconsistency in this system.",
+          "I would imagine that most people would learn to use this system very quickly.",
+          "I found the system very cumbersome to use.",
+          "I felt very confident using the system.",
+          "I needed to learn a lot of things before I could get going with this system."
         ].map((question, index) => ({
           title: question,
           value: sus[index],
